71fe38864b5421a5cea605a4b7bfab4bc32c29af
shiba mitsue <mitue.okamoto@gmail.com>
2024-12-27 12:17:54 +0900

    gh-pages

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base:process.env.NODE_ENV === 'production' ? '/apng-maker/' : './',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
